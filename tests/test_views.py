d843e678a78007c785332ee262c3592fda3e42b8
retroam <retroam@yahoo.com>
2014-05-19 13:36:12 -0400

    skeleton for test_views

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,175 +1,68 @@
-import mock
+"""Views tests for the Mendeley addon."""
+
+import os
 import unittest
 from nose.tools import *
+import mock
+import httplib
+import datetime
+
+from werkzeug import FileStorage
 from webtest_plus import TestApp
-
-import website.app
-from tests.base import DbTestCase
-
-from tests.factories import ProjectFactory, AuthUserFactory
-
-from website.addons.base import AddonError
-from website.addons.mendeley.tests.utils import create_mock_mendeley
-from website.addons.mendeley import views
-
-
 from framework.auth.decorators import Auth
-from website.addons.mendeley import settings as mendeley_settings
+from website.util import api_url_for
+from website.project.model import NodeLog
+from tests.base import DbTestCase, URLLookup, assert_is_redirect
+from tests.factories import AuthUserFactory
 
 
-app = website.app.init_app(
-    routes=True, set_backends=False, settings_module='website.settings'
+from website.addons.mendeley.tests.utils import (
+    MendeleyAddonTestCase, app, mock_responses, MockMendeley
 )
 
-mendeley_mock = create_mock_mendeley(project=436)
+lookup = URLLookup(app)
+mock_client = MockMendeley()
 
-
-class TestViewsConfig(DbTestCase):
-
-    def setUp(self):
-
-        super(TestViewsConfig, self).setUp()
-
-        self.app = TestApp(app)
-        self.user = AuthUserFactory()
-        self.consolidated_auth = Auth(user=self.user)
-        self.auth = ('test', self.user.api_keys[0]._primary_key)
-        self.project = ProjectFactory(creator=self.user)
-
-        self.non_authenticator = AuthUserFactory()
-        self.project.add_contributor(
-            contributor=self.non_authenticator,
-            auth=Auth(self.project.creator),
-        )
-
-        self.project.add_addon('mendeley', auth=self.consolidated_auth)
-        self.project.creator.add_addon('mendeley')
-        self.node_settings = self.project.get_addon('mendeley')
-        self.user_settings = self.project.creator.get_addon('mendeley')
-        self.user_settings.oauth_access_token = 'legittoken'
-        self.user_settings.oauth_access_token_secret = 'legittoken'
-        self.user_settings.save()
-        self.node_settings.user_settings = self.user_settings
-        self.node_settings.mendeley_id = '123456'
-        self.node_settings.mendeley_type = 'project'
-        self.node_settings.mendeley_title = 'OVER9000'
-        self.node_settings.save()
-
-        self.mendeley = create_mock_mendeley('test')
-
-    def test_config_no_change(self):
-        num = len(self.project.logs)
-        url = '/api/v1/project/{0}/mendeley/settings/'.format(self.project._id)
-        rv = self.app.post_json(
-            url, {'mendeley_value': 'project_123456', 'mendeley_title': 'OVER9000'}, auth=self.user.auth)
-        self.project.reload()
-
-        assert_equal(rv.status_int, 200)
-        assert_equal(len(self.project.logs), num)
-
-    def test_config_change(self):
-        num = len(self.project.logs)
-        url = '/api/v1/project/{0}/mendeley/settings/'.format(self.project._id)
-        rv = self.app.post_json(
-            url, {'mendeley_value': 'project_9001', 'mendeley_title': 'IchangedbecauseIcan'}, auth=self.user.auth)
-        self.project.reload()
-        self.node_settings.reload()
-
-        assert_equal(rv.status_int, 200)
-        assert_equal(self.node_settings.mendeley_id, '9001')
-        assert_equal(len(self.project.logs), num + 1)
-        assert_equal(self.project.logs[num].action, 'mendeley_content_linked')
-
-    def test_config_unlink(self):
-        url = '/api/v1/project/{0}/mendeley/unlink/'.format(self.project._id)
-        rv = self.app.post(url, auth=self.user.auth)
-        self.node_settings.reload()
-        self.project.reload()
-
-        assert_equal(self.project.logs[-1].action, 'mendeley_content_unlinked')
-        assert_equal(rv.status_int, 200)
-        assert_true(self.node_settings.mendeley_id == None)
-
-    def test_config_unlink_no_node(self):
-        self.node_settings.user_settings = None
-        self.node_settings.save()
-        self.node_settings.reload()
-        url = '/api/v1/project/{0}/mendeley/unlink/'.format(self.project._id)
-        rv = self.app.post(url, expect_errors=True, auth=self.user.auth)
-        self.project.reload()
-
-        assert_equal(self.node_settings.mendeley_id, '123456')
-        assert_not_equal(self.project.logs[-1].action, 'mendeley_content_unlinked')
-        assert_equal(rv.status_int, 400)
-
-
-class TestUtils(DbTestCase):
+class TestAuthViews(DbTestCase):
 
     def setUp(self):
-        super(TestUtils, self).setUp()
+        return None
 
-        self.app = TestApp(app)
-        self.user = AuthUserFactory()
-        self.consolidated_auth = Auth(user=self.user)
-        self.auth = ('test', self.user.api_keys[0]._primary_key)
-        self.project = ProjectFactory(creator=self.user)
+    def test_mendeley_oauth_start(self):
+        return None
 
-        self.non_authenticator = AuthUserFactory()
-        self.project.add_contributor(
-            contributor=self.non_authenticator,
-            auth=Auth(self.project.creator),
-        )
+    def test_mendeley_oauth_finish(self):
+        return None
 
-        self.project.add_addon('mendeley', auth=self.consolidated_auth)
-        self.project.creator.add_addon('mendeley')
-        self.node_settings = self.project.get_addon('mendeley')
-        self.user_settings = self.project.creator.get_addon('mendeley')
-        self.user_settings.oauth_access_token = 'legittoken'
-        self.user_settings.oauth_access_token_secret = 'legittoken'
-        self.user_settings.save()
-        self.node_settings.user_settings = self.user_settings
-        self.node_settings.mendeley_id = '436'
-        self.node_settings.mendeley_type = 'project'
-        self.node_settings.save()
+    def test_mendeley_oauth_delete_user(self):
+        return None
 
 
+class TestConfigViews(MendeleyAddonTestCase):
+
+    def test_mendeley_config_get(self):
+        return None
+
+    def test_mendeley_deauthorize(self):
+        return None
+
+    def test_mendeley_import_user_auth_add_a_log(self):
+        return None
+
+    def test_mendeley_get_share_emails(self):
+        return None
+
+    def test_mendeley_get_share_emails_returns_error_if_not_authorize(self):
+        return None
+
+    def test_mendeley_get_share_emails_requires_user_addon(self):
+        return None
+
+class TestFilebrowserViews(MendeleyAddonTestCase):
+
+    def test_mendeley_addon_folder(self):
+        return None
 
 
 
 
-class TestViewsAuth(DbTestCase):
-
-    def setUp(self):
-        super(TestViewsAuth, self).setUp()
-
-        self.app = TestApp(app)
-        self.user = AuthUserFactory()
-        self.consolidated_auth = Auth(user=self.user)
-        self.auth = ('test', self.user.api_keys[0]._primary_key)
-        self.project = ProjectFactory(creator=self.user)
-
-        self.non_authenticator = AuthUserFactory()
-        self.project.add_contributor(
-            contributor=self.non_authenticator,
-            auth=Auth(self.project.creator),
-        )
-
-        self.project.add_addon('mendeley', auth=self.consolidated_auth)
-        self.project.creator.add_addon('mendeley')
-        self.node_settings = self.project.get_addon('mendeley')
-        self.user_settings = self.project.creator.get_addon('mendeley')
-        self.node_settings.user_settings = self.user_settings
-        self.node_settings.mendeley_id = '436'
-        self.node_settings.mendeley_type = 'project'
-        self.node_settings.save()
-
-    #TODO Finish me, would require a lot of mocking it seems.
-    def test_oauth_fail(self):
-        url = '/api/v1/project/{0}/mendeley/oauth'.format(self.project._id)
-        rv = self.app.get(url, auth=self.user.auth).maybe_follow()
-        pass
-
-
-    #TODO Finish me
-    def test_oauth_bad_token(self):
-        pass
